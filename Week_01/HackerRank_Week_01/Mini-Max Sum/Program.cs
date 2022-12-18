ddc0a729a822ea06d160b2120ef7ff7b6e798744
v-kirilov <v_kirilov87@abv.bg>
2022-12-18 11:59:23 +0200

    Min Max Sum Task

---
+++ Week_01/HackerRank_Week_01/Mini-Max Sum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Collections.Generic;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long maxSum = 0;
+        long minSum = 0;
+
+        arr = arr.OrderBy(x => x).ToList();
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (i < arr.Count - 1)
+            {
+                minSum += arr[i];
+            }
+            if (i > 0)
+            {
+                maxSum += arr[i];
+            }
+        }
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
